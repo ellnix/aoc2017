18fa928e36b8f2b4e207465d41396e8230a1f411
ellnix <103502144+ellnix@users.noreply.github.com>
2024-05-14 15:41:41 +0200

    Update runner with more memory and mem debug fn

--- runner.cjs
+++ runner.cjs
@@ -4,7 +4,7 @@ const fs = require('fs');
 const wasm_bytes = fs.readFileSync(`solutions/${day_num}.wasm`);
 const input_bytes = new Uint8Array(fs.readFileSync(`inputs/${day_num}`));
 
-let memory = new WebAssembly.Memory({initial: Math.ceil(input_bytes.length * 4 / 64)});
+let memory = new WebAssembly.Memory({initial: 2});
 let memory_buffer = new Uint8Array(memory.buffer);
 for(let i = 0; i < input_bytes.length; i++) {
   memory_buffer[i] = input_bytes[i]
@@ -16,6 +16,12 @@ WebAssembly.instantiate(
     env: {
       input: memory,
       debug_func: console.log,
+      print_memory_32: (from, len) => {
+        const buffer = new Uint32Array(memory.buffer);
+        const data = buffer.slice(from, from + len);
+
+        console.log(data);
+      }
     }
   }
 ).then(wasm_module => {
